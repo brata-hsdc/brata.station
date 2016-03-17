86640b502c8e01c2e739437772205c7bc32d4a0a
Ellery Chan <ellery@chanspace.org>
2016-03-17 02:25:17 -0400

    Add bash scripts to run sim and show text.
    run_flight.sh runs a dock sim with the parameters specified on
    the command line.
    
    show_text.sh displays multiple lines of text in a specified color and size.
    
    Also added auto-text scaling for the kiosk text.
    
    Fixed a bug where Text.MIDDLE justification was being ignored and would
    default to Text.TOP.

---
+++ station/test/run_flight.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Run a canned flight simulation
+#
+# Usage: run_flight.sh t_aft t_coast t_fore team_name [sim_duration]
+
+station_addr=http://localhost:5000/rpi
+
+# Simulation runtime (default 10 sec)
+# and sleep times between sending the commands
+sim_duration=${5:-10}
+start_sleep_s=10
+sim_sleep_s=$(($sim_duration + 10))
+reset_sleep_s=3
+
+# Command line args
+t_aft=$1
+t_coast=$2
+t_fore=$3
+team_name=${4:-Mystery Team}
+
+# Send start_challenge:  go to Welcome screen
+http --json POST $station_addr/start_challenge Content-type:application/json Accept:application/json team_name="$team_name" > /dev/null
+sleep $start_sleep_s
+
+# Send post_challenge:  run the sim, show the result
+http --json POST $station_addr/post_challenge Content-type:application/json Accept:application/json t_aft="$t_aft" t_coast="$t_coast" t_fore="$t_fore" a_aft='.15' a_fore='0.09' r_fuel='0.7' q_fuel='20' dist='15.0' v_min='0.01' v_max='0.1' v_init='0.0' t_sim="$sim_duration" > /dev/null
+sleep $sim_sleep_s
+
+# Send reset:  go to Ready screen
+http --json GET $station_addr/reset/31415 Content-type:text/html > /dev/null
+sleep $reset_sleep_s
---
+++ station/test/show_text.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Display a screenful of text
+#
+# Usage: show_text.sh 'Multi-line|Text' [duration_s [COLOR [size [(x,y)]]]]
+#
+# duration_s  defaults to 10 sec.
+# COLOR       must be WHITE, BLACK, RED, GREEN, BLUE, YELLOW,
+#                CYAN, MAGENTA, ORANGE, LIGHT_ORANGE; defaults to WHITE
+# size        is the height of a capital letter in pixels; defaults to 100
+# (x,y)       is the position of the center of the text; defaults to (960,500)
+
+station_addr=http://localhost:5000/rpi/start_challenge
+
+# Get command line args
+text=$1
+duration_s=${2:-10}
+color=${3:-WHITE}
+size=${4:-100}
+pos=${5:-(960,540)}
+
+# Send message to display the text, then wait for the specified duration
+http --json POST $station_addr Content-type:application/json Accept:application/json kiosk_text="(($size, Colors.$color, $pos, Text.CENTER|Text.MIDDLE, \"$text\"),)" > /dev/null
+sleep $duration_s
